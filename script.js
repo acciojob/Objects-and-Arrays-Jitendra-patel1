b0f1a83cf9ad1e4d99071691f64756699b610c37
Jitendra-patel1 <125756903+Jitendra-patel1@users.noreply.github.com>
2023-08-11 12:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ const person = {
 };
 
 const team=players
-const team1=["John", "Bob", "Alice", "Poppy"];
-const cap1=person
+const team1=[];
+for(int i=0; i<players.length; i++){
+	cap1.push(players[i]);
+}
+
+function copyObject(source) {
+  const copy = {};
+  for (let key in source) {
+    if (source.hasOwnProperty(key)) {
+      copy[key] = source[key];
+    }
+  }
+  return copy;
+}
+
+const cap1 = copyObject(person);
+
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
